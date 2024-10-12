cdb4940d15d445f7431e0e4a1e05e7922af66db6
ForgeDom <maxstill2006@gmail.com>
2024-10-12 17:16:12 +0300

    +

--- main.cpp
+++ main.cpp
@@ -95,6 +95,99 @@ public:
         cout << endl;
     }
 
+    void InsertAtPosition(T val, int pos) {
+        if (pos < 1) {
+            cout << "Неможлива позицыя!" << endl;
+            return;
+        }
+        Node<T>* newNode = new Node<T>(val);
+        if (pos == 1) {
+            AddToHead(val);
+            return;
+        }
+        Node<T>* temp = head;
+        for (int i = 1; i < pos - 1 && temp; ++i) {
+            temp = temp->next;
+        }
+        if (!temp) {
+            cout << "Позиція виходить за межі списку!" << endl;
+            delete newNode;
+            return;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        if (newNode->next == nullptr) {
+            tail = newNode;
+        }
+    }
+
+    void DeleteAtPosition(int pos) {
+        if (pos < 1 || !head) {
+            cout << "Неможлива позиція або список порожній!" << endl;
+            return;
+        }
+        if (pos == 1) {
+            DeleteFromHead();
+            return;
+        }
+        Node<T>* temp = head;
+        for (int i = 1; i < pos - 1 && temp->next; ++i) {
+            temp = temp->next;
+        }
+        if (!temp->next) {
+            cout << "Позиція виходить за межі списку!" << endl;
+            return;
+        }
+        Node<T>* nodeToDelete = temp->next;
+        temp->next = temp->next->next;
+        if (!temp->next) {
+            tail = temp;
+        }
+        delete nodeToDelete;
+    }
+
+    int Search(T val) {
+        Node<T>* temp = head;
+        int pos = 1;
+        while (temp) {
+            if (temp->data == val) {
+                return pos;
+            }
+            temp = temp->next;
+            ++pos;
+        }
+        return 0; 
+    }
+
+    int SearchAndReplace(T oldVal, T newVal) {
+        Node<T>* temp = head;
+        int count = 0;
+        while (temp) {
+            if (temp->data == oldVal) {
+                temp->data = newVal;
+                ++count;
+            }
+            temp = temp->next;
+        }
+        return count > 0 ? count : -1; 
+    }
+
+    void Reverse() {
+        if (!head || !head->next) {
+            return;
+        }
+        Node<T>* prev = nullptr;
+        Node<T>* curr = head;
+        tail = head;
+        while (curr) {
+            Node<T>* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
     ~SinglyLinkedList() {
         DeleteAll();
     }
@@ -111,13 +204,32 @@ int main() {
     list.AddToTail(30);
     list.Show();
 
-    list.DeleteFromHead();
+    list.InsertAtPosition(25, 3);
     list.Show();
 
-    list.DeleteFromTail();
+    list.DeleteAtPosition(2);
     list.Show();
 
-    list.DeleteAll();
+    int pos = list.Search(30);
+    if (pos) {
+        cout << "Елемент 30 знайдено на позиції: " << pos << endl;
+    }
+    else {
+        cout << "Елемент 30 не знайдено!" << endl;
+    }
+
+    int replacedCount = list.SearchAndReplace(25, 50);
+    if (replacedCount != -1) {
+        cout << "Кількість замінених елементів: " << replacedCount << endl;
+    }
+    else {
+        cout << "Елемент для заміни не знайдено!" << endl;
+    }
+
+    list.Show();
+
+    list.Reverse();
+    cout << "Список після перевертання: ";
     list.Show();
 
     return 0;
